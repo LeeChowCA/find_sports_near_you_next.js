d7fce5cd15e3f444cc49cac1310a24b1581e2cf1
Lee <zhoujianpingls@outlook.com>
2025-02-19 21:49:58 -0800

    profile sub domain added, the user can also delete the card they created

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,19 @@ import React from 'react'
 import Image from 'next/image'
 import { HiOutlinePencilSquare,  HiArrowLeftOnRectangle } from "react-icons/hi2";
 import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/navigation';
 
 const USER_IMAGE = 'https://miro.medium.com/v2/resize:fit:640/format:webp/1*B3Z3Knd8M0zXj8zMPp3n-w.jpeg'
 
 const Header = () => {
-
+  const router=useRouter();
   const {data: session} = useSession();
 
   return (
     <div className='flex justify-between p-3 border-b-[2px] border-[#FF3366]'>
-      <Image alt='logo' src='/images/logo.png' width={120} height={120}/>
+      <Image alt='logo' src='/images/logo.png' width={120} height={120} className='hover:cursor-pointer' onClick={() => router.push('/')}/>
       <div className='flex gap-4'>
-        <button className='bg-black p-2 px-3 text-white rounded-full'>
+        <button onClick={()=>router.push('/create-post')} className='bg-black p-2 px-3 text-white rounded-full'>
           <span className='hidden sm:block'>CREATE POST</span>
           <HiOutlinePencilSquare  className='sm:hidden text-[20px]'/>
         </button>
@@ -26,7 +27,7 @@ const Header = () => {
           <span className='hidden sm:block'>Sign In</span>
           <HiArrowLeftOnRectangle className='sm:hidden'/>
           </button>}
-        <Image src={session ? session?.user?.image: USER_IMAGE} alt='user image' width={45} height={40} className='rounded-full'/>
+        <Image src={session ? session?.user?.image: USER_IMAGE} alt='user image' width={45} height={40} className='rounded-full hover:cursor-pointer' onClick={() => router.push('/profile')}/>
       </div>
     </div>
   )
